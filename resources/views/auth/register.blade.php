9ffaa02965ec7e53e3b289e6689033f1930a35a2
Umesh <umeshchetan1@gmail.com>
2024-02-14 15:12:29 +0530

    latest changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,139 +3,126 @@
 @section('register')
 <p>Register</p>
 <div class="container">
-    <div class="row">
-        <div class="col-md-6">
-            <form class="form-horizontal" action="{{route('postregister')}}" method="post" id="multi_step_form">
-                @csrf
-                <div class="tab-content">
-                    <!-- Step 1: User Details -->
-                    <div class="tab-pane active" id="user_details">
-                        <!-- User Details Form Fields -->
-                        <!-- Input fields for username and password -->
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="name">Name*:</label>
-                            <div class="col-sm-8">
-                                <input type="text" name="name" id="name" class="form-control form-control-sm">
-                            </div>
-                        </div>
-                        <br />
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="email">Email*:</label>
-                            <div class="col-sm-8">
-                                <input type="email" name="email" id="email" class="form-control form-control-sm">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="phone">phone*:</label>
-                            <div class="col-sm-8">
-                                <input type="number" name="phone" id="phone" class="form-control form-control-sm">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="date_of_birth">date_of_birth*:</label>
-                            <div class="col-sm-8">
-                                <input type="date" name="date_of_birth" id="date_of_birth"
-                                    class="form-control form-control-sm">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="Password">Password*:</label>
-                            <div class="col-sm-8">
-                                <input type="password" id="pass" name="password" class="form-control form-control-sm">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="confirm_password">Confirm Password*:</label>
-                            <div class="col-sm-8">
-                                <input type="password" id="confirm_password" name="confirm_password"
-                                    class="form-control form-control-sm">
-                            </div>
-                        </div>
-                        <button type="button" class="btn-sm btn-primary" id="btn_next_user_details">Next</button>
-                    </div>
-
-                    <!-- Step 2: Personal Details -->
-                    <div class="tab-pane" id="personal_details">
-                        <!-- Personal Details Form Fields -->
-                        <!-- Input fields for phone number and address -->
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="Address">Address*:</label>
-                            <div class="col-sm-8">
-                                <textarea name="address" class="form-control" rows="5"></textarea>
-                            </div>
-                        </div>
-                        <br>
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="country">Country*:</label>
-                            <div class="col-sm-8">
-                                <input type="text" id="country" name="country" class="form-control form-control-sm">
-                            </div>
-                        </div>
-                        <br>
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="state">State*:</label>
-                            <div class="col-sm-8">
-                                <input type="text" id="state" name="state" class="form-control form-control-sm">
-                            </div>
-                        </div>
-                        <br>
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="city">City*:</label>
-                            <div class="col-sm-8">
-                                <input type="text" id="city" name="city" class="form-control form-control-sm">
-                            </div>
-                        </div>
-                        <br>
-                        <button type="button" name="btn_previous_personal_details" id="btn_previous_personal_details"
-                            class="btn btn-info btn-sm">Previous</button>
-                        <button type="button" class="btn-sm btn-primary" id="btn_next_personal_details">Next</button>
-
-                    </div>
-                    <div class="tab-pane" id="app_details">
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="Zip">Zip*:</label>
-                            <div class="col-sm-8">
-                                <input type="text" id="Zip" name="Zip" class="form-control form-control-sm">
-                            </div>
-                        </div>
-                        <br>
-                        <div class="form-group">
-                            <label class="control-label col-sm-2"
-                                for="how_do_you_know_about_garcia">how_do_you_know_about_garcia*:</label>
-                            <div class="col-sm-8">
-                                <select name="how_do_you_know_about_garcia" id="how_do_you_know_about_garcia">
-                                    <option value="select">Select</option>
-                                    <option value="facebook">Facebook</option>
-                                    <option value="twitter">Twitter</option>
-                                    <option value="youtube">YouTube</option>
-                                </select>
-                            </div>
-                        </div>
-                        <br>
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="terms_and_conditions">Zip*:</label>
-                            <div class="col-sm-8">
-                                <input type="checkbox" name="terms_and_conditions" class="mb-3">
-                                You must agree with our Terms and conditions
-                            </div>
-                        </div>
-                        <br>
-                        <button type="button" name="previous_btn_personal_details" id="btn_previous_app_details"
-                            class="btn btn-info btn-sm">Previous</button>
-                        <button type="submit" class="btn-sm btn-primary" name="save">Save</button>
+    <form class="form-horizontal" action="{{route('postregister')}}" method="post" id="multi_step_form">
+        @csrf
+        <div class="tab-content">
+            <!-- Step 1: User Details -->
+            <div class="tab-pane active" id="user_details">
+                <!-- User Details Form Fields -->
+                <!-- Input fields for username and password -->
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="name">Name*:</label>
+                    <div class="col-sm-8">
+                        <input type="text" name="name" id="name" class="form-control form-control-sm">
                     </div>
                 </div>
-            </form>
-            <div class="signFoot m-4">
-                <label for="">Already have an account?
-                    <a id="loginAnchor" href="">Login</a>
-                </label>
+                <br />
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="email">Email*:</label>
+                    <div class="col-sm-8">
+                        <input type="email" name="email" id="email" class="form-control form-control-sm">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="phone">phone*:</label>
+                    <div class="col-sm-8">
+                        <input type="number" name="phone" id="phone" class="form-control form-control-sm">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="date_of_birth">date_of_birth*:</label>
+                    <div class="col-sm-8">
+                        <input type="date" name="date_of_birth" id="date_of_birth" class="form-control form-control-sm">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="Password">Password*:</label>
+                    <div class="col-sm-8">
+                        <input type="password" id="pass" name="password" class="form-control form-control-sm">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="confirm_password">Confirm Password*:</label>
+                    <div class="col-sm-8">
+                        <input type="password" id="confirm_password" name="confirm_password"
+                            class="form-control form-control-sm">
+                    </div>
+                </div>
+                <button type="button" class="btn-sm btn-primary" id="btn_next_user_details">Next</button>
+            </div>
+
+            <!-- Step 2: Personal Details -->
+            <div class="tab-pane" id="personal_details">
+                <!-- Personal Details Form Fields -->
+                <!-- Input fields for phone number and address -->
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="Address">Address*:</label>
+                    <div class="col-sm-8">
+                        <textarea name="address" class="form-control" rows="5"></textarea>
+                    </div>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="country">Country*:</label>
+                    <div class="col-sm-8">
+                        <input type="text" id="country" name="country" class="form-control form-control-sm">
+                    </div>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="state">State*:</label>
+                    <div class="col-sm-8">
+                        <input type="text" id="state" name="state" class="form-control form-control-sm">
+                    </div>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="city">City*:</label>
+                    <div class="col-sm-8">
+                        <input type="text" id="city" name="city" class="form-control form-control-sm">
+                    </div>
+                </div>
+                <br>
+                <button type="button" name="previous_btn_personal_details" id="btn_previous_personal_details"
+                    class="btn btn-info btn-sm">Previous</button>
+                <button type="button" class="btn-sm btn-primary" id="btn_next_personal_details">Next</button>
+
+            </div>
+            <div class="tab-pane" id="app_details">
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="Zip">Zip*:</label>
+                    <div class="col-sm-8">
+                        <input type="text" id="Zip" name="Zip" class="form-control form-control-sm">
+                    </div>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="control-label col-sm-2"
+                        for="how_do_you_know_about_garcia">how_do_you_know_about_garcia*:</label>
+                    <div class="col-sm-8">
+                        <select name="how_do_you_know_about_garcia" id="how_do_you_know_about_garcia">
+                            <option value="select">Select</option>
+                            <option value="facebook">Facebook</option>
+                            <option value="twitter">Twitter</option>
+                            <option value="youtube">YouTube</option>
+                        </select>
+                    </div>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="terms_and_conditions">Zip*:</label>
+                    <div class="col-sm-8">
+                        <input type="checkbox" name="terms_and_conditions" class="mb-3">
+                        You must agree with our Terms and conditions
+                    </div>
+                </div>
+                <br>
+                <button type="button" name="previous_btn_personal_details" id="previous_btn_app_details"
+                    class="btn btn-info btn-sm">Previous</button>
+                <button type="submit" class="btn-sm btn-primary" name="save">Save</button>
             </div>
         </div>
-        <div class="col-md-6">
-            
-        </div>
-    </div>
+    </form>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"
@@ -143,7 +130,7 @@
 
 <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.js"></script>
 <script>
-    $(document).ready(function () {
+   $(document).ready(function () {
         // Initialize form validation on the multi-step form
         $("#multi_step_form").validate({
             // Specify validation rules for each step
@@ -211,27 +198,31 @@
         });
     });
     $(document).ready(function () {
-        // Step 1 to Step 2
-        $("#btn_next_user_details").click(function                      if ($("#multi_step_form").va                             $("#user_details").removeClas
-        $("#personal_details").add            ct        );
-                }
-        });
+         // Step 1 to Step 2 Navigation
+    $("#btn_next_user_details").click(function () {
+        if ($("#multi_step_form").valid()) {
+            $("#user_details").removeClass("active");
+            $("#personal_details").addClass("active");
+        }
+    });
 
-    //        p 2 to Step 1 Navigation
-    $("#btn_previous_person            ls").click(function () {
-        $("#perso            ils").removeClass("active");
-        "#use        tails").addClass("active");
-            });
+    // Step 2 to Step 1 Navigation
+    $("#btn_previous_personal_details").click(function () {
+        $("#personal_details").removeClass("active");
+        $("#user_details").addClass("active");
+    });
 
-        // Step 2 to Step 3 Navigation
-                   _next_personal_details").click(function () {
-    $("#personal_details").removeClass("active                        $("#app_details").addClass("        ve");
-        });
+    // Step 2 to Step 3 Navigation
+    $("#btn_next_personal_details").click(function () {
+        $("#personal_details").removeClass("active");
+        $("#app_details").addClass("active");
+    });
 
     // Step 3 to Step 2 Navigation
-    $("#btn_previous_app_details").click(function ()                   $("#app_details").removeClass("activ        
-            l_details").addClass("active");
-        });
+    $("#previous_btn_app_details").click(function () {
+        $("#app_details").removeClass("active");
+        $("#personal_details").addClass("active");
+    });
     });
 </script>
 @endsection
